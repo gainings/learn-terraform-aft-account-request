08d284ef2656c35ab01020fb374d26e2114c412b
gainings <gain.beats@gmail.com>
2025-03-08 22:03:22 +0900

    4

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,3 +47,28 @@ module "sandbox_aft_03" {
   }
   account_customizations_name = ""
 }
+
+module "sandbox_aft_04" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "gain.beats+sandbox_aft4@gmail.com"
+    AccountName               = "sandbox-aft-04"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = "NOT"
+    SSOUserLastName           = "USE"
+  }
+
+  account_tags = {
+    "Project"     = "test"
+    "Environment" = "Production"
+    "AFT"         = true
+  }
+
+  change_management_parameters = {
+    change_requested_by = "chadain"
+    change_reason       = "Test AFT 4"
+  }
+  account_customizations_name = ""
+}
