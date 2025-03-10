279c25f713b8c1fd831dbbae149c9bea0110c165
gainings <gain.beats@gmail.com>
2025-03-10 19:17:10 +0900

    9

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
-module "sandbox_aft_08" {
+module "sandbox_aft_09" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "gain.beats+sandbox_aft8@gmail.com"
-    AccountName               = "sandbox-aft-08"
+    AccountEmail              = "gain.beats+sandbox_aft9@gmail.com"
+    AccountName               = "sandbox-aft-09"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "gain.beats+aft@gmail.com"
     SSOUserFirstName          = "NOT"
@@ -18,7 +18,7 @@ module "sandbox_aft_08" {
 
   change_management_parameters = {
     change_requested_by = "chadain"
-    change_reason       = "Test AFT 8"
+    change_reason       = "Test AFT 9"
   }
   account_customizations_name = ""
 }
